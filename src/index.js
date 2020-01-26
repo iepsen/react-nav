683c7b3fc5f1c71769146f49c580f908d2463a1e
Marcelo Iepsen <marcelo.iepsen@accedo.tv>
2020-01-26 11:53:00 +0100

    Passing down isActive prop in a simplified way

--- src/index.js
+++ src/index.js
@@ -9,19 +9,13 @@ import { getContainerStyle } from './css'
 ReactDOM.render(
   <div style={getContainerStyle()}>
     <Nav id="header" onDown="body" active>
-      {isActive => (
-        <Header isActive={isActive} />
-      )}
+      {isActive => <Header isActive={isActive} />}
     </Nav>
     <Nav id="body" onUp="header" onDown="footer">
-      {isActive => (
-        <Body isActive={isActive} />
-      )}
+      {isActive => <Body isActive={isActive} />}
     </Nav>
     <Nav id="footer" onUp="body">
-      {isActive => (
-        <Footer isActive={isActive} />
-      )}
+      {isActive => <Footer isActive={isActive} />}
     </Nav>
   </div>,
   document.getElementById('app')
