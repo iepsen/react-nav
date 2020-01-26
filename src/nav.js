491a558181d17f74e11c13b1b3ca2224f3c88799
Marcelo Iepsen <marcelo.iepsen@accedo.tv>
2020-01-26 11:24:30 +0100

    Fix docstring

--- src/nav.js
+++ src/nav.js
@@ -4,13 +4,14 @@ import useNav from './use-nav'
 /**
  * 
  * @param {Object} props
- * @param {any} props.children
+ * @param {Function} props.children
  * @param {string} props.id
  * @param {string} [props.onUp]
  * @param {string} [props.onDown]
  * @param {string} [props.onLeft]
  * @param {string} [props.onRight]
  * @param {boolean} [props.active]
+ * @returns {React.ReactElement}
  */
 const Nav = ({ children, ...props}) => {
   const isActive = useNav(props)
